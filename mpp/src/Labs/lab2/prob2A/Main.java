4f4bb7934a3c51f11db798ae0d3cffa3b87357b8
devraj <devendra3v@gmail.com>
2025-10-01 22:34:48 -0500

    problem 2 of lab 2 review

--- mpp/src/Labs/lab2/prob2A/Main.java
+++ mpp/src/Labs/lab2/prob2A/Main.java
@@ -15,25 +15,45 @@ class Student {
    public String getName(){
         return name;
    }
+   public void setName(String name){
+        this.name=name;
+   }
+   @Override
+    public String toString(){
+        return name+" : "+gradeReport;
+   }
 }
 
 class GradeReport {
     private Student student;
+    private String grade;
     GradeReport(Student student) {
         this.student=student;
     }
     public Student getStudent(){
         return student;
     }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+    @Override
+    public String toString(){
+        return grade;
+    }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Bob");
-        GradeReport gradeReport = new GradeReport(student);
-        System.out.println("Grade Report of "+gradeReport.getStudent().getName());
-        System.out.println(student.getGradeReport().getStudent().getName());
+        Student student = new Student("Devendra");
+        student.getGradeReport().setGrade("A+");
+        System.out.println(student);
+        System.out.println(student.getGradeReport());
 
     }
 }
